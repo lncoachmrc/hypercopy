c43883d87498c64a6dfc1b121b662a6dda8062bc
lncoachmrc <130929841+lncoachmrc@users.noreply.github.com>
2026-08-23 16:47:54 +0200

    Initialize frontend language system

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,5 +3,13 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import {AuthProvider} from './auth';
+import {initLanguage} from './i18n';
+import {initAutoTranslate} from './autoTranslate';
 import './styles.css';
+import './i18n.css';
+
+initLanguage();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><BrowserRouter><AuthProvider><App/></AuthProvider></BrowserRouter></React.StrictMode>);
+
+initAutoTranslate();
